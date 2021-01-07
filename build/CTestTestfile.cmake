61f6b89f900ce2a8670c36b9d8146abb43771b7c
Mihai Anton <antonmihai58@gmail.com>
2021-01-07 11:55:08 +0200

    Removes files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mihai/Documents/School/TUDelft/DataVisualisation/volvis/Framework
-# Build directory: /Users/mihai/Documents/School/TUDelft/DataVisualisation/volvis/Framework/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("integrity_tests")
--- build/integrity_tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mihai/Documents/School/TUDelft/DataVisualisation/volvis/Framework/integrity_tests
-# Build directory: /Users/mihai/Documents/School/TUDelft/DataVisualisation/volvis/Framework/build/integrity_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
